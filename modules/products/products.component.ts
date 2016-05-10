0847baa9015625a5cb7ece3e2c9c9d17a60a2e1f
Jacques Vincilione <jvincilione@gmail.com>
2016-05-10 16:31:33 -0400

    Add download feature for report.

--- modules/products/products.component.ts
+++ modules/products/products.component.ts
@@ -153,6 +153,71 @@ class ProductsComponent implements OnInit, OnChanges {
             });
     }
 
+    downloadReport() {
+        //If JSONData is not an object then JSON.parse will parse the JSON string in an Object
+        let arrData = this.reportData.logs;
+        let reportTitle = this.timeframe + ' Report for: ' + arrData.productName;
+        
+        let CSV = '';    
+        //Set Report title in first row or line
+        
+        CSV += ReportTitle + '\r\n\n';
+
+        let row = "";
+        
+        //This loop will extract the label from 1st index of on array
+        for (let index in arrData[0]) {
+            
+            //Now convert each value to string and comma-seprated
+            //do not add index for $$hashkey or key
+            if (index !== '$$hashKey' && index !== 'key' && index !== "status") {
+                row += index + ',';
+            }
+        }
+
+        row = row.slice(0, -1);
+        
+        //append Label row with line break
+        CSV += row + '\r\n';
+        
+        //1st loop is to extract each row
+        for (let i = 0; i < arrData.length; i++) {
+            let row = "";
+            
+            //2nd loop will extract each column and convert it in string comma-seprated
+            for (let index in arrData[i]) {
+                if (index !== '$$hashKey' && index !== 'key' && index !== "status") {
+                    row += '"' + arrData[i][index] + '",';
+                }
+            }
+
+            row.slice(0, row.length - 1);
+            
+            //add a line break after each row
+            CSV += row + '\r\n';
+        }
+        
+        //Generate a file name
+        let fileName = reportTitle.replace(/ /g,"_"); 
+        
+        //Initialize file format you want csv or xls
+        let uri = 'data:text/csv;charset=utf-8,' + escape(CSV); 
+        
+        //this trick will generate a temp <a /> tag
+        let link = document.createElement("a");    
+        link.href = uri;
+        
+        //set the visibility hidden so it will not effect on your web-layout
+        link.style = "visibility:hidden";
+        link.download = fileName + ".csv";
+        
+        //this part will append the anchor tag and remove it after automatic click
+        //auto click will download the file
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
+
     dismissError() {
         this.error = {};
     }
